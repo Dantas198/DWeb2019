de6ec54e63238b8ef9777110d8e4ebcf260c20ac
Dantas <xp1998@live.com.pt>
2019-11-24 01:57:31 +0000

    multiple files in api

---
+++ TP10/ficheiros/routes/api.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs')
+var Ficheiros = require('../controllers/ficheiros')
+var Ficheiro = require('../models/ficheiros')
+
+
+var multer = require('multer')
+var upload = multer({dest: 'uploads/'})
+
+/* GET users listing. */
+router.get('/ficheiros', function(req, res) {
+  Ficheiros.listar()
+          .then(dados => res.jsonp(dados))
+          .catch(erro => res.status(500).jsonp(erro))
+});
+
+router.post('/ficheiros', upload.array('ficheiro'), function(req,res){
+  let data = new Date()
+for(var i = 0; i<req.files.length; i++){
+  let oldPath = __dirname + '/../' + req.files[i].path
+  let newPath = __dirname + '/../public/ficheiros/' + req.files[i].originalname
+
+  fs.rename(oldPath, newPath, function (err){
+    if (err) throw err
+  })
+    if(req.files.length == 1)
+      var descricao = req.body.desc
+    else 
+      var descricao = req.body.desc[i]
+    let novoFicheiro = new Ficheiro(
+      {
+        data: data.toISOString(),
+        desc: descricao,
+        name: req.files[i].originalname,
+        mimetype: req.files[i].mimetype,
+        size: req.files[i].size
+      })
+    
+    novoFicheiro.save(function( err, ficheiro){
+      if (!err) {console.log("Ficheiro guardado com sucesso")
+    
+    }
+      else console.log(err)
+    })
+  }
+  res.redirect('/')
+})
+
+module.exports = router;
